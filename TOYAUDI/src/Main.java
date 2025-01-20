eee06f3adc0aac10010754070887b700e5bdc1d2
JustFallBack <fall.f777mdk@gmail.com>
2025-01-20 23:58:40 +0100

    Removed Main test class.

--- TOYAUDI/src/Main.java
+++
@@ -1,9 +0,0 @@
-import vehicle.NumberOfDoors;
-
-public class Main {
-    public static void main(String[] args) {
-        for (NumberOfDoors type : NumberOfDoors.values()) {
-            System.out.println(type + ": " + type.getNumberOfDoors() + " doors vehicle, " + type.getDescription());
-        }
-    }
-}
